dca4e5efbb47d5c88dc8443058a270051b052254
unknown <mohit.wadhawan@PC-260.ids-logic.local>
2015-07-08 18:19:50 +0530

    8th July 2015

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'marcaec');
+define('DB_NAME', 'marsaec');
 
 /** MySQL database username */
-define('DB_USER', 'marcaec');
+define('DB_USER', 'marsaec');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'ids@123456');
+define('DB_PASSWORD', 'mars@123456');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
